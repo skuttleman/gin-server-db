63d15c3c5decb70e6d6d6bff4051510fb296bdb7
Ben Allred <skuttleman@gmail.com>
2016-02-07 13:38:29 -0700

    deployment

--- routes/books.go
+++ routes/books.go
@@ -1,19 +1,19 @@
 package routes
 
 import (
-  "github.com/gin-gonic/gin"
+	"github.com/skuttleman/gin-server/Godeps/_workspace/src/github.com/gin-gonic/gin"
 )
 
 func Books(i *gin.RouterGroup) {
-  i.GET("/", func(c *gin.Context) {
-    c.JSON(200, gin.H {
-      "books": "list",
-    })
-  })
-  i.GET("/:id", func(c *gin.Context) {
-    c.JSON(200, gin.H {
-      "book": c.Param("id"),
-      // "foo": c.Get("foo"),
-    })
-  })
+	i.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"books": "list",
+		})
+	})
+	i.GET("/:id", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"book": c.Param("id"),
+			// "foo": c.Get("foo"),
+		})
+	})
 }
--- routes/index.go
+++ routes/index.go
@@ -1,19 +1,19 @@
 package routes
 
 import (
-  "github.com/gin-gonic/gin"
+	"github.com/skuttleman/gin-server/Godeps/_workspace/src/github.com/gin-gonic/gin"
 )
 
 func Index(i *gin.RouterGroup) {
-  i.GET("/", func(c *gin.Context) {
-    c.JSON(200, gin.H {
-      "main": "list",
-    })
-  })
-  i.GET("/:id", func(c *gin.Context) {
-    c.JSON(200, gin.H {
-      "main": c.Param("id"),
-      // "foo": c.Get("foo"),
-    })
-  })
+	i.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"main": "list",
+		})
+	})
+	i.GET("/:id", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"main": c.Param("id"),
+			// "foo": c.Get("foo"),
+		})
+	})
 }
